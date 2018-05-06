a6705bcf70281769e444b1399d0a7545e2c43839
Ben L <benlertlumprasertkul@gmail.com>
2018-05-06 17:52:57 +0100

    support multiple touch devices and multiple touches

--- src/index.js
+++ src/index.js
@@ -12,14 +12,17 @@ const app = new PIXI.Application({
 const graphics = new PIXI.Graphics();
 app.stage.addChild(graphics);
 
-socket.on('pointer location', function(locations) {
+socket.on('pointer location', function(locationsById) {
 
   graphics.clear();
   graphics.beginFill(0xFF3300);
 
-  for (let id in locations) {
-    let { x, y } = locations[id];
-    graphics.drawCircle(x, y, 5);
+  for (let id in locationsById) {
+    let locations = locationsById[id];
+    for (let i = 0; i < locations.length; i++) {
+      const { x, y } = locations[i];
+      graphics.drawCircle(x, y, 5);
+    }
   }
 
   graphics.endFill();
@@ -32,12 +35,27 @@ function resize() {
 function emitPointerLocation(event) {
   const { x, y } = event;
 
-  socket.emit('pointer location', { x, y });
+  event.preventDefault();
+  socket.emit('pointer location', [{ x, y }]);
+}
+
+function emitTouchLocation(event) {
+  const locations = [];
+
+  for (let i = 0; i < event.touches.length; i++) {
+    const { pageX: x, pageY: y } = event.touches[i];
+    locations.push({ x, y });
+  }
+
+  event.preventDefault();
+  socket.emit('pointer location', locations);
 }
 
 const throttledEmitPointerLocation = throttle(emitPointerLocation, MAX_FPS);
+const throttledEmitTouchLocation = throttle(emitTouchLocation, MAX_FPS);
 
-window.addEventListener("resize", resize, false);
-window.addEventListener("pointermove", throttledEmitPointerLocation, false);
+app.view.addEventListener("resize", resize, false);
+app.view.addEventListener("pointermove", throttledEmitPointerLocation, false);
+app.view.addEventListener("touchmove", throttledEmitTouchLocation, false);
 
 document.body.appendChild(app.view);
