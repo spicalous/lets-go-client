3d5c15727e3c753574c2f3b8685915e03d231c25
Ben L <benlertlumprasertkul@gmail.com>
2018-05-06 17:02:20 +0100

    specify host

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,6 +7,7 @@ const webpackConfig = require('./webpack.config.js');
 module.exports = merge(webpackConfig, {
   devtool: 'eval-source-map',
   devServer: {
+    host: '0.0.0.0',
     contentBase: './dist'
   },
   plugins: [
