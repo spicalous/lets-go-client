c1b03fadb4f3baf86f3e94be1b9fd1a13b4315a7
Ben L <benlertlumprasertkul@gmail.com>
2018-05-06 16:57:49 +0100

    show pointer locations

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import * as PIXI from "pixi.js";
 import io from "socket.io-client";
+import throttle from "lodash/throttle";
 
-let app = new PIXI.Application({
+const socket = io(`${window.location.hostname}:3000`);
+const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight
+});
+const graphics = new PIXI.Graphics();
+app.stage.addChild(graphics);
+
+socket.on('pointer location', function(locations) {
+
+  graphics.clear();
+  graphics.beginFill(0xFF3300);
+
+  for (let id in locations) {
+    let { x, y } = locations[id];
+    graphics.drawCircle(x, y, 5);
+  }
+
+  graphics.endFill();
 });
 
 function resize() {
   app.renderer.resize(window.innerWidth, window.innerHeight);
 }
 
+function emitPointerLocation(event) {
+  const { x, y } = event;
+
+  socket.emit('pointer location', { x, y });
+}
+
+const throttledEmitPointerLocation = throttle(emitPointerLocation, 100, { trailing: true });
+
 window.addEventListener("resize", resize, false);
+window.addEventListener("pointermove", throttledEmitPointerLocation, false);
 
 document.body.appendChild(app.view);
-
-const socketIOURL = `${window.location.hostname}:3000`
-const socket = io(socketIOURL);
