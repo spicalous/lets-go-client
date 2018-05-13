5f33b11bb31ab6256e61e49f93183738e0f83460
Ben L <benlertlumprasertkul@gmail.com>
2018-05-13 02:39:03 +0100

    emit create game event

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import io from "socket.io-client";
 
 const socket = io(`${window.location.hostname}:3000`);
 
+function onCreateGame() {
+  socket.emit("create game");
+}
+
 window.onload = function() {
 
-  const bodyElement = document.querySelector('body');
+  const createButton = document.querySelector("#btn-id-create");
 
+  createButton.addEventListener("click", onCreateGame);
 }
