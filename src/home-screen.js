57072a16fa753da215766f15e9858678f648fc14
Ben L <benlertlumprasertkul@gmail.com>
2018-05-15 00:58:09 +0100

    join existing game

--- src/home-screen.js
+++ src/home-screen.js
@@ -7,7 +7,7 @@ class HomeScreen {
     this._createButton = document.querySelector("#btn-id-create");
 
     this._socket.on("game list", this._onGameList.bind(this));
-    this._socket.on("game created", this._onGameCreated.bind(this));
+    this._socket.on("game created", this._enterGame.bind(this));
     this._createButton.addEventListener("click", this._createGame.bind(this));
 
     this._socket.emit("get games list");
@@ -22,11 +22,13 @@ class HomeScreen {
     gameList.forEach((game) => {
       let roomListItem = document.createElement("div");
       roomListItem.innerHTML = game.id;
+      roomListItem.className = "room-item";
+      roomListItem.addEventListener("click", this._enterGame.bind(this, game));
       this._roomContainer.append(roomListItem);
     });
   }
 
-  _onGameCreated(game) {
+  _enterGame(game) {
     window.location = `${window.location}game?id=${game.id}`;
   }
 }
