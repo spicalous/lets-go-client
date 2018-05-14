fecba066a430a396d38334b14d055865aee06765
Ben L <benlertlumprasertkul@gmail.com>
2018-05-15 00:42:36 +0100

    added HomeScreen class

---
+++ src/home-screen.js
@@ -0,0 +1,36 @@
+
+class HomeScreen {
+
+  constructor(socket) {
+    this._socket = socket;
+    this._roomContainer = document.querySelector(".room-container");
+    this._createButton = document.querySelector("#btn-id-create");
+
+    this._socket.on("game list", this._onGameList.bind(this));
+    this._socket.on("game created", this._onGameCreated.bind(this));
+    this._createButton.addEventListener("click", this._createGame.bind(this));
+
+    this._socket.emit("get games list");
+  }
+
+  _createGame() {
+    this._socket.emit("create game");
+  }
+
+  _onGameList(gameList) {
+
+    console.log(gameList);
+
+    gameList.forEach((game) => {
+      let roomListItem = document.createElement("div");
+      roomListItem.innerHTML = game.id;
+      this._roomContainer.append(roomListItem);
+    });
+  }
+
+  _onGameCreated(game) {
+    console.log(game);
+  }
+}
+
+export default HomeScreen;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import io from "socket.io-client";
+import HomeScreen from "./home-screen";
 
 const socket = io(`${window.location.hostname}:3000`);
 
-function onCreateGame() {
-  socket.emit("create game");
-}
-
 window.onload = function() {
-
-  const createButton = document.querySelector("#btn-id-create");
-
-  createButton.addEventListener("click", onCreateGame);
+  const homeScreen = new HomeScreen(socket);
 }
