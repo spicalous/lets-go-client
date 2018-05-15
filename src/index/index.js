2c1d7b283189faad50d1e8c18d2a33fca6f05668
Ben L <benlertlumprasertkul@gmail.com>
2018-05-15 22:19:55 +0100

    use DOMContentLoaded event

--- src/index/index.js
+++ src/index/index.js
@@ -3,6 +3,9 @@ import IndexScreen from "./index-screen";
 
 const socket = io(`${window.location.hostname}:3000`);
 
-window.onload = function() {
+function onDOMContentLoaded() {
   const indexScreen = new IndexScreen(socket);
+  document.removeEventListener("DOMContentLoaded", onDOMContentLoaded);
 }
+
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
