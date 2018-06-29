7a8936c07d8cb18d4cecdaa8773fb82c9d9cd6bf
Ben L <benlertlumprasertkul@gmail.com>
2018-06-30 00:52:13 +0100

    move single run option into config

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -2,6 +2,7 @@ const webpackConfig = require("./webpack.config.js");
 
 module.exports = function(config) {
   config.set({
+    singleRun: true,
     browsers: ["ChromeHeadless"],
     frameworks: ["mocha", "chai"],
     files: ["test/unit/**/*-test.js"],
